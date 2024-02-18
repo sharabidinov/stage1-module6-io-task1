cc1e8b1f6b4981de0bf41fc0e331452c589eab12
Alymbek Sharabidinov <73030640+sharabidinov@users.noreply.github.com>
2024-02-19 00:01:20 +0600

    solution

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,17 +2,12 @@ package com.epam.mjc.io;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class FileReader {
-
-    public static void main(String[] args) {
-        File file = new File("/Users/sharabidinov/esc/stage-1/stage1-module6-io-task1/src/main/resources/Profile.txt");
-        FileReader fileReader = new FileReader();
-        fileReader.getDataFromFile(file);
-    }
 
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
@@ -24,13 +19,18 @@ public class FileReader {
                 data.add(line.split(": ")[1]);
                 line = in.readLine();
             }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException");
         } catch (IOException e) {
-            System.out.println("Please Try Again");
+            System.out.println("IOException");
         }
+
         profile.setName(data.get(0));
         profile.setAge(Integer.parseInt(data.get(1)));
         profile.setEmail(data.get(2));
         profile.setPhone(Long.parseLong(data.get(3)));
+
         return profile;
     }
 }
